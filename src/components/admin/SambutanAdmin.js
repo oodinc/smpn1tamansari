213a55d101768793bfc67bf219b3c45499a0e307
oodinc <masoodinc1@gmail.com>
2025-01-16 19:36:14 +0700

    remove unused header from SambutanAdmin component

--- src/components/admin/SambutanAdmin.js
+++ src/components/admin/SambutanAdmin.js
@@ -56,9 +56,6 @@ const SambutanAdmin = () => {
   return (
     <div className="bg-gray-50 py-12">
       <div className="max-w-7xl mx-auto p-4">
-        <h3 className="text-2xl font-medium text-gray-700 mb-4">
-          Admin - Kelola Sambutan
-        </h3>
         <h2 className="text-4xl font-semibold text-center mb-8 text-gray-800">
           Admin - Kelola Pengumuman Sekolah
         </h2>
